e54e112e2289c15373048585d3fc6bce8514ae64
jahid-111 <jahidjob5@outlook.com>
2025-02-17 00:47:20 +0300

    QuerySearch

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const express = require("express");
 const projectData = require("./src/route/project");
 const skillsData = require("./src/route/skills");
 const expertiseData = require("./src/route/expertise");
+const queryData = require("./src/route/querySearch");
 const dbConnect = require("./services/mongoDb/dbConnection");
 dbConnect();
 const app = express();
 app.use("/project", projectData);
 app.use("/skills", skillsData);
 app.use("/expertise", expertiseData);
+app.use("/", queryData);
 
 app.listen(3000, () => {
   console.log("Server is running on ➡︎❤️‍🔥http://localhost:3000/project");
