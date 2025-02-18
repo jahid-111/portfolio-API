d0962c60d6ee6854e2878040355606bd678cdcd7
jahid-111 <jahidjob5@outlook.com>
2025-02-19 02:31:52 +0300

    Refactor API routes to include '/api' prefix and update server port configuration

--- index.js
+++ index.js
@@ -6,25 +6,28 @@ const expertiseData = require("./src/route/expertise");
 const queryData = require("./src/route/querySearch");
 const blogData = require("./src/route/blog");
 
-const errorHandler = require("./src/utils/errorHandle");
+const errorHandler = require("./src/utils/errorHandler");
 const dbConnect = require("./services/mongoDb/dbConnection");
 dbConnect();
 
 const app = express();
+
+const PORT = process.env.PORT || 3000;
+
 app.use(cors());
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/project", projectData);
-app.use("/skills", skillsData);
-app.use("/expertise", expertiseData);
-app.use("/blog", blogData);
-app.use("/search", queryData);
+app.use("/api/project", projectData);
+app.use("/api/skills", skillsData);
+app.use("/api/expertise", expertiseData);
+app.use("/api/blog", blogData);
+app.use("/api/search", queryData);
 
 // Use the error handler middleware after all routes
 app.use(errorHandler);
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log("Server is running on ➡︎❤️‍🔥http://localhost:3000/project");
 });
